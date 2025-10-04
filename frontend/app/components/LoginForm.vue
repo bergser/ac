80d8d2bf9329c37100a7051ddb60e09573b17c2b
Sergei Berg <sksdes@gmail.com>
2025-10-04 13:38:22 +0200

    introduce generic authentication

---
+++ frontend/app/components/LoginForm.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const password = ref("");
+const email = ref("");
+const { authenticate } = useUserStore();
+</script>
+<template>
+  <UCard>
+    <form @submit.prevent="authenticate(email, password)">
+      <UInput v-model="email" />
+      <UInput type="password" v-model="password" />
+      <UButton type="submit" label="Login" />
+    </form>
+  </UCard>
+</template>
--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const userStore = useUserStore();
+const { user } = storeToRefs(userStore);
+await userStore.fetchUser();
+</script>
 
 <template>
   <div>
     <UContainer class="sm:border-x border-default pt-10">
       <slot />
+      <LoginForm v-if="!user" />
+      <div v-else>
+        <UButton @click="userStore.signOut">Sign out</UButton>
+      </div>
     </UContainer>
   </div>
 </template>
